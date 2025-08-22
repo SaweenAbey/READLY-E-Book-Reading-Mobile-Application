2d102e38a58260d459ca121de65ad655e4400d98
SaweenAbey <saween910@gmail.com>
2025-08-23 02:10:23 +0530

    Update Homepage

--- app/src/main/java/com/example/readly_ebook/Signup.kt
+++ app/src/main/java/com/example/readly_ebook/Signup.kt
@@ -1,6 +1,8 @@
 package com.example.readly_ebook
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class Signup : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val nextButton = findViewById<Button>(R.id.btnSignUp)
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, Loging::class.java)
+            startActivity(intent)
         }
+
     }
 }
